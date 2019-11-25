3a07418bda45932efcc8c13ea2f67c7f9c21eb2a
ilufa168card <57581278+ilufa168card@users.noreply.github.com>
2019-11-25 14:13:59 +0700

    Update and rename ttmsantai.php to ttmregis.php

---
+++ ttmregis.php
@@ -0,0 +1,49 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+include "function.php";
+echo color("nevy","Creator : □ TTM Crew Anti WiuWiuW □\n");
+echo "□ Time    :".date('[d-m-Y] [H:i:s]')." □\n\n";
+// function change(){
+        $nama = nama();
+        $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
+        ulang:
+        echo color("nevy","□ MASUKKAN NOMOR ANDA BOSS: ");
+        // $no = trim(fgets(STDIN));
+        $nohp = trim(fgets(STDIN));
+        $nohp = str_replace("62","62",$nohp);
+        $nohp = str_replace("(","",$nohp);
+        $nohp = str_replace(")","",$nohp);
+        $nohp = str_replace("-","",$nohp);
+        $nohp = str_replace(" ","",$nohp);
+        if (!preg_match('/[^+0-9]/', trim($nohp))) {
+            if (substr(trim($nohp),0,3)=='62') {
+                $hp = trim($nohp);
+            }
+            else if (substr(trim($nohp),0,1)=='0') {
+                $hp = '62'.substr(trim($nohp),1);
+        }
+         elseif(substr(trim($nohp), 0, 2)=='62'){
+            $hp = '6'.substr(trim($nohp), 1);
+        }
+        else{
+            $hp = '1'.substr(trim($nohp),0,13);
+        }
+    }
+        $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$hp.'","signed_up_country":"ID"}';
+        $register = request("/v5/customers", null, $data);
+        if(strpos($register, '"otp_token"')){
+        $otptoken = getStr('"otp_token":"','"',$register);
+        echo color("blue","□ CEK OTP DI HAPE")."\n";
+        otp:
+        echo color("nevy","□ MASUKKAN KODE OTP: ");
+        $otp = trim(fgets(STDIN));
+        $data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
+        $verif = request("/v5/customers/phone/verify", null, $data1);
+        if(strpos($verif, '"access_token"')){
+        echo color("green","□ AKUN ANDA BERHASIL TERDAFTAR BOSS\n");
+        $token = getStr('"access_token":"','"',$verif);
+        $uuid = getStr('"resource_owner_id":',',',$verif);
+        echo color("nevy","□ access token : ".$token."\n\n");
+        save("token.txt",$token);
+//  }
+// echo change()."\n";
